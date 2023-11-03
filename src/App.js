7ea6484cff3302b80e82758ab5cce3373c6d36de
HussainShafayet <shafayetsec09@gmail.com>
2023-11-03 11:07:20 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Gallery from './components/gallery';
 
 function App() {
   return (
     <div className='bg-primary '>
-      Image 
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImageCard from './ImageCard';
+
+export default function Gallery() {
+  return (
+    <div>
+        <ImageCard />
+        <ImageCard />
+    </div>
+  )
+}
