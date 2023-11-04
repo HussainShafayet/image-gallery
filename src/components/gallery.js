c5ac197dc656d64ebb9df53046d6c8c731cd11d0
HussainShafayet <shafayetsec09@gmail.com>
2023-11-04 18:51:02 +0600

    iamge edit done and selected count update

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -4,56 +4,80 @@ import '.././assets/css/gallery.css';
 import API_BASE_URL from '../apiConfig';
 
 const Gallery = () => {
-      const [images, setImages] = useState([]);
-      const [selectedCount, setSelectCount] = useState(0);
+    const [images, setImages] = useState([]);
+    const [selectedCount, setSelectedCount] = useState(
+    images.filter((image) => image.isSelect).length
+    );
+    
 
-      useEffect((()=>{
-        let url = `${API_BASE_URL}/imagepost/`;
+    useEffect((()=>{
+    let url = `${API_BASE_URL}/imagepost/`;
+    const requestOptions = {
+        method:"GET",
+        headers:{"Content-Type":"application/json"}
+    }
+    fetch(url,requestOptions)
+    .then((res)=>res.json())
+    .then((response)=>{
+        console.log('res',response.data);
+        setImages(response.data);
+    })
+    }),[]);
+
+    useEffect(() => {
+    const count = images.filter((image) => image.isSelect).length;
+    setSelectedCount(count);
+    }, [images]);
+    
+    
+    const handleAddImage = async (e)=>{
+        e.preventDefault();
+        let url =  `${API_BASE_URL}/imagepost/`;
+
+        const formData = new FormData();
+        formData.append('file',  e.target.files[0]);
         const requestOptions = {
-          method:"GET",
-          headers:{"Content-Type":"application/json"}
-        }
-        fetch(url,requestOptions)
-        .then((res)=>res.json())
-        .then((response)=>{
-          console.log('res',response.data);
-          setImages(response.data)
-        })
-      }),[]);
-       
-        const handleAddImage = async (e)=>{
-            e.preventDefault();
-            let url =  `${API_BASE_URL}/imagepost/`;
-
-            const formData = new FormData();
-            formData.append('file',  e.target.files[0]);
-            const requestOptions = {
-                method: "POST",
-                body: formData,
-            };
-            fetch(url,
-                requestOptions
-            )
-                .then((res) => res.json())
-                .then((response) => {
-                console.log('data',response)
-
-                // add the new image to the list
-                setImages((prevImages) => [...prevImages, response.data]);
-            });
-            }
-
-          const handleImageClick = (imageId) => {
-            const updatedImages = images.map((image) => {
-            if (image.id === imageId) {
-                image.isSelect = !image.isSelect;
-            }
-            return image;
-            });
-
-            setImages(updatedImages);
-            calculateTotal()
+            method: "POST",
+            body: formData,
         };
+        fetch(url,
+            requestOptions
+        )
+            .then((res) => res.json())
+            .then((response) => {
+            console.log('data',response)
+
+            // add the new image to the list
+            setImages((prevImages) => [...prevImages, response.data]);
+        });
+        }
+
+    const handleImageClick = (image) => {
+        let url =  `${API_BASE_URL}/imageedit/`;
+    
+        const requestOptions = {
+            method: "PUT",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify({id:image.id,isSelect: !image.isSelect}),
+        };
+        fetch(url,
+            requestOptions
+        )
+            .then((res) => res.json())
+            .then((response) => {
+            console.log('data edit',response)
+
+            //udpate images list
+            const updatedImageData = response.data;
+            setImages((prevImages) =>
+                prevImages.map((image) =>
+                image.id === updatedImageData.id ? updatedImageData : image
+                )
+            );
+            calculateTotal(images);
+        
+        });
+    };
 
     const handleDeleteSelected = async () => {
         let deleteList = [];
@@ -66,42 +90,40 @@ const Gallery = () => {
         });
         if (deleteList.length>0) {
             let url =  `${API_BASE_URL}/imagedelete/`;
-       
-        const requestOptions = {
-            method: "DELETE",
-            headers:{"Content-Type":"application/json"},
-            body: JSON.stringify(deleteList),
-        };
-        fetch(url,
-            requestOptions
-        )
-            .then((res) => res.json())
-            .then((response) => {
-            console.log('data',response)
+    
+            const requestOptions = {
+                method: "DELETE",
+                headers:{"Content-Type":"application/json"},
+                body: JSON.stringify(deleteList),
+            };
+            fetch(url,
+                requestOptions
+            )
+                .then((res) => res.json())
+                .then((response) => {
+                console.log('data',response)
 
-            //udpate images list
-            setImages(updatedImageList);
-            const totalItemCount = updatedImageList.filter((image) => image.isSelect).length;
-            setSelectCount(totalItemCount);
-           
-        });
+                //udpate images list
+                setImages(updatedImageList);
+                calculateTotal(updatedImageList)
+            
+            });
         }
         
     };
 
-    
-    const calculateTotal = () => {
-		const totalItemCount = images.filter((image) => image.isSelect).length;
-        console.log(totalItemCount,images.filter((image) => image.isSelect));
-		setSelectCount(totalItemCount);
-	};
+
+    const calculateTotal = (data) => {
+        const totalItemCount = data.filter((image) => image.isSelect).length;
+        setSelectedCount(totalItemCount);
+    };
   return (
     <div>
     Total Selected:{selectedCount} 
     <button onClick={handleDeleteSelected}>Delete Selected</button>
         <div className="gridLayout">
         {images.map((item)=>
-            <div key={item.id} className={`${item.isFeatured ? 'child' : ''}`}  onClick={() => handleImageClick(item.id)} >
+            <div key={item.id} className={`${item.isFeatured ? 'child' : ''}`}  onClick={() => handleImageClick(item)} >
             <ImageCard isSelect={item.isSelect} imageUrl={API_BASE_URL + item.file} />
             </div>
             )}
