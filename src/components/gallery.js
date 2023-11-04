8648233b8900915fb3d988c402a0aff39ecbb97c
HussainShafayet <shafayetsec09@gmail.com>
2023-11-04 20:17:20 +0600

    home page UI update

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -119,22 +119,38 @@ const Gallery = () => {
     };
   return (
     <div>
-    Total Selected:{selectedCount} 
-    <button onClick={handleDeleteSelected}>Delete Selected</button>
-        <div className="gridLayout">
-        {images.map((item)=>
-            <div key={item.id} className={`${item.isFeatured ? 'child' : ''}`}  onClick={() => handleImageClick(item)} >
-            <ImageCard isSelect={item.isSelect} imageUrl={API_BASE_URL + item.file} />
-            </div>
-            )}
-            <div>
-                <label htmlFor="imageInput" className="custom-file-upload card" >
-                    <input type="file" id="imageInput" accept="image/*" onChange={handleAddImage} />
-                        Upload Image
-                </label>
+        <div className='d-flex justify-content-center align-items-center w-100 mt-2'>
+            <div className='galleryArea shadow bg-white rounded'>
+                <div className='p-3 d-flex justify-content-between align-items-center'>
+                    <div className="form-check d-flex justify-content-start align-items-center">
+                        <input className="form-check-input mt-0" type="checkbox" checked={(selectedCount>0)? true : false} readOnly />
+                        <label className="form-check-label fw-bold" style={{marginLeft:6 +'px'}} htmlFor="flexCheckChecked">
+                            {selectedCount} Files Selected
+                        </label>
+                    </div>
+
+                    {selectedCount>0 && 
+                        <span className="text-danger fw-bold" style={{cursor:'pointer'}} onClick={handleDeleteSelected}>Delete Files</span>
+                    }
+                    
+                </div>
+                <hr className='m-0'/>
+
+                <div className="gridLayout">
+                    {images.map((item, index)=>
+                    <div key={item.id} className={`${index==0 ? 'child' : ''}`}  onClick={() => handleImageClick(item)} >
+                    <ImageCard isSelect={item.isSelect} imageUrl={API_BASE_URL + item.file} />
+                    </div>
+                    )}
+                    <div>
+                        <label htmlFor="imageInput" className="custom-file-upload card" >
+                            <input type="file" id="imageInput" accept="image/*" onChange={handleAddImage} />
+                                Upload Image
+                        </label>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
   )
 }
